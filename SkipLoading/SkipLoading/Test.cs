6e415c569dcd14af777b3fcef179d1515b814d3a
liviustefan <liviustefan.petroaia@gmail.com>
2020-08-03 21:17:43 +0300

    It will be something great

---
+++ SkipLoading/SkipLoading/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkipLoading
+{
+    public class Test
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
